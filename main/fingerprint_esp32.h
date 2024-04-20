3e31a13a9ad007c93a1562f06047de75470721e7
Joagz <joagomez.dev@gmail.com>
2024-04-20 17:51:42 -0300

    manejando los datos de la huella (fingerprint_esp32.h)

--- main/fingerprint_esp32.h
+++ main/fingerprint_esp32.h
@@ -1,14 +1,130 @@
+#include "Adafruit_Fingerprint.h"
+#define DATA_SIZE 67
+
 class RegisterRoutine
 {
 private:
     bool registerMode;
-public:
 
-    RegisterRoutine(bool on)
+public:
+    RegisterRoutine()
     {
-        this->registerMode = on;
+        this->registerMode = false;
     }
 
-    bool getRegisterRoutine(){  return this->registerMode;  }
-    void setRegisterRoutine(bool on){ this->registerMode = on; };
+public:
+    bool getRegisterRoutine() { return this->registerMode; }
+    void setRegisterRoutine(bool on) { this->registerMode = on; };
 };
+
+class FingerprintSensorRoutine
+{
+
+private:
+    Adafruit_Fingerprint finger;
+
+public:
+    FingerPrintSensorRoutine(HardwareSerial *SerialPort)
+    {
+        this->finger = Adafruit_Fingerprint(SerialPort);
+        finger.begin(FINGERPRINT_BAUDRATE_115200);
+
+        if (finger.verifyPassword())
+        {
+            Serial.println("Sensor de huella encontrado!");
+        }
+        else
+        {
+            Serial.println("Sensor de huella no encontrado...");
+        }
+
+        finger.getParameters();
+        Serial.print(F("Status: 0x"));
+        Serial.println(finger.status_reg, HEX);
+        Serial.print(F("Sys ID: 0x"));
+        Serial.println(finger.system_id, HEX);
+        Serial.print(F("Capacity: "));
+        Serial.println(finger.capacity);
+        Serial.print(F("Security level: "));
+        Serial.println(finger.security_level);
+        Serial.print(F("Device address: "));
+        Serial.println(finger.device_addr, HEX);
+        Serial.print(F("Packet len: "));
+        Serial.println(finger.packet_len);
+        Serial.print(F("Baud rate: "));
+        Serial.println(finger.baud_rate);
+    }
+
+    uint64_t *getActualFingerData()
+    {
+        Adafruit_Fingerprint_Packet packet;
+        uint8_t p = this->finger.getImage();
+        switch (p)
+        {
+        case FINGERPRINT_OK:
+            Serial.println("Image taken");
+            break;
+        case FINGERPRINT_NOFINGER:
+            Serial.println("No finger detected");
+            return p;
+        case FINGERPRINT_PACKETRECIEVEERR:
+            Serial.println("Communication error");
+            return p;
+        case FINGERPRINT_IMAGEFAIL:
+            Serial.println("Imaging error");
+            return p;
+        default:
+            Serial.println("Unknown error");
+            return p;
+        }
+
+        uint8_t p = this->finger.getStructuredPacket(&packet);
+
+        switch (p)
+        {
+        case FINGERPRINT_OK:
+            // recibimos el packete y devolvemos sus datos
+
+            return data;
+        default:
+            break;
+        }
+    }
+}
+
+// wrap data inside a packet
+class DataPacket
+{
+
+private:
+    u_char data[67];
+    size_t dataSize;
+
+public:
+
+    size_t dataSize()
+    {
+        return this.dataSize;
+    }
+    u_char *data()
+    {
+        return this.data;
+    }
+
+    DataPacket(uint8_t *data)
+    {
+        u_char wrapped_data[DATA_SIZE] = {0x00};
+
+        wrapped_data[0] = 0x01;
+
+        for (int i = 1, j = 0; j < DATA_SIZE - 3; i++, j++)
+            wrapped_data[i] = data[j];
+
+        wrapped_data[DATA_SIZE-2] = 0x04;
+        wrapped_data[DATA_SIZE-1] = 0x00;
+
+        this->data = wrapped_data;
+        this->dataSize = sizeof(wrapped_data);
+
+    }
+}
--- main/main.cpp
+++ main/main.cpp
@@ -22,7 +22,8 @@
 // las libs de Arduino, como Adafruit_Fingerprint.
 // Vamos a usarla en profundidad en el proyecto
 #include "Arduino.h"
-#include "Adafruit_Fingerprint.h"
+// Incluido en fingerprint_esp32.h
+// #include "Adafruit_Fingerprint.h"
 #include "HardwareSerial.h"
 
 #include "freertos/event_groups.h"
@@ -41,14 +42,17 @@
 #include "sys/ioctl.h"
 #include "fingerprint_esp32.h"
 
-
+#ifndef __GNUC__
 #pragma region DEFINES
+#endif
 
 /** DEFINICIONES **/
-static const RegisterRoutine registerRoutine;
 
-// Usamos los pines 16 y 17 en UART2 
-HardwareSerial SerialPort(2);
+// Usamos los pines 16 y 17 en UART2
+HardwareSerial SerialPort = HardwareSerial(2);
+
+RegisterRoutine registerRoutine = RegisterRoutine();
+FingerprintSensorRoutine fingerPrintSensorRoutine = FingerprintSensorRoutine(&SerialPort);
 
 #define WIFI_SUCCESS 1 << 0
 #define WIFI_FAILURE 1 << 1
@@ -70,11 +74,11 @@ static const char *WIFI_PASS = "123";
         * SET_MODE_REGISTER: (desde el cliente) string que indica al servidor para cambiar de modo a "registrar"
         * SET_MODE_AUTH: (desde el cliente) string que indica al servidor para cambiar de modo a "autenticar"
 */
-static const char RECV_OK[] = { 0x01, 'R', 'E', 'C', 'V', 'O', 'K', 0x04, 0x00 };
-static const char RECV_BAD[] = { 0x01, 'R', 'E', 'C', 'V', 'B', 'A', 'D', 0x04, 0x00 };
-static const char SET_MODE_REGISTER[] = { 0x01, 'R', 'E', 'G', 0x04, 0x00 };
-static const char SET_MODE_AUTH[] = { 0x01, 'A', 'U', 'T', 'H', 0x04, 0x00 };
-static const char DISCONNECT[] = { 0x01, 'F', 'I', 'N', 0x04, 0x00 };
+static const u_char RECV_OK[] = {0x01, 'R', 'E', 'C', 'V', 'O', 'K', 0x04, 0x00};
+static const u_char RECV_BAD[] = {0x01, 'R', 'E', 'C', 'V', 'B', 'A', 'D', 0x04, 0x00};
+static const u_char SET_MODE_REGISTER[] = {0x01, 'R', 'E', 'G', 0x04, 0x00};
+static const u_char SET_MODE_AUTH[] = {0x01, 'A', 'U', 'T', 'H', 0x04, 0x00};
+static const u_char DISCONNECT[] = {0x01, 'F', 'I', 'N', 0x04, 0x00};
 
 static const uint8_t RECB_SIZE = 10;
 static const uint8_t RECO_SIZE = 9;
@@ -84,7 +88,7 @@ static const char SOH = 0x01; /* "START OF HEAD" */
 static const char EOT = 0x04; /* "END OF TRANSMISSION "*/
 
 #ifndef SERVER_PORT
-    #define SERVER_PORT 4321
+#define SERVER_PORT 4321
 #endif
 
 /** FIN DEFINICIONES **/
@@ -98,81 +102,91 @@ static int s_retry_num = 0;
 static const char *TAG = "WIFI";
 static const char *SERVER_TAG = "SERVER_ESP32";
 
+#ifndef __GNUC__
 #pragma endregion
+#endif
 
 /** FUNCIONES **/
 
+#ifndef __GNUC__
 #pragma region HANDLERS
+#endif
 
 // Manejar eventos de WiFi
-static void wifi_event_handler(void* arg, esp_event_base_t event_base,
-                                int32_t event_id, void* event_data)
+static void wifi_event_handler(void *arg, esp_event_base_t event_base,
+                               int32_t event_id, void *event_data)
 {
-	if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START)
-	{
-		ESP_LOGI(TAG, "Conectando...");
-		esp_wifi_connect();
-	} else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED)
-	{
-		if (s_retry_num < MAX_FAILURES)
-		{
-			ESP_LOGI(TAG, "Reconectando...");
-			esp_wifi_connect();
-			s_retry_num++;
-		} else {
-			xEventGroupSetBits(wifi_event_group, WIFI_FAILURE);
-		}
-	}
+    if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START)
+    {
+        ESP_LOGI(TAG, "Conectando...");
+        esp_wifi_connect();
+    }
+    else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED)
+    {
+        if (s_retry_num < MAX_FAILURES)
+        {
+            ESP_LOGI(TAG, "Reconectando...");
+            esp_wifi_connect();
+            s_retry_num++;
+        }
+        else
+        {
+            xEventGroupSetBits(wifi_event_group, WIFI_FAILURE);
+        }
+    }
 }
 
 // Manejar eventos de IP (cuando se le asigna una IP al dispositivo, al conectarse a la red
 // este evento es lanzado. Se ejecuta luego de que esp_wifi_connect() sea exitoso).
-static void ip_event_handler(void* arg, esp_event_base_t event_base,
-                                int32_t event_id, void* event_data)
+static void ip_event_handler(void *arg, esp_event_base_t event_base,
+                             int32_t event_id, void *event_data)
 {
-	if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP)
-	{
-        ip_event_got_ip_t* event = (ip_event_got_ip_t*) event_data;
+    if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP)
+    {
+        ip_event_got_ip_t *event = (ip_event_got_ip_t *)event_data;
         ESP_LOGI(TAG, "STA IP: " IPSTR, IP2STR(&event->ip_info.ip));
         s_retry_num = 0;
         xEventGroupSetBits(wifi_event_group, WIFI_SUCCESS);
     }
-
 }
+
+#ifndef __GNUC__
 #pragma endregion
+#endif
 
-
-
+#ifndef __GNUC__
 #pragma region WIFI
+#endif
+
 // Conectarse a WIFI y obtener el resultado
 esp_err_t connect_wifi()
 {
-	int status = WIFI_FAILURE;
+    int status = WIFI_FAILURE;
 
-	/** INICIALIZAR COSAS **/
-	// Interfaz de Red de ESP
-	ESP_ERROR_CHECK(esp_netif_init());
+    /** INICIALIZAR COSAS **/
+    // Interfaz de Red de ESP
+    ESP_ERROR_CHECK(esp_netif_init());
 
-	// Event Loop por defecto
-	ESP_ERROR_CHECK(esp_event_loop_create_default());
+    // Event Loop por defecto
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
 
-	// Wifi Station en el driver
-	esp_netif_create_default_wifi_sta();
+    // Wifi Station en el driver
+    esp_netif_create_default_wifi_sta();
 
-	// Iniciar Wifi Station con la config. predeterminada
-	wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    // Iniciar Wifi Station con la config. predeterminada
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
     ESP_ERROR_CHECK(esp_wifi_init(&cfg));
 
     /** Iniciar el grupo de evento de wifi **/
-	wifi_event_group = xEventGroupCreate();
+    wifi_event_group = xEventGroupCreate();
 
     // Estas funciones registran una instancia del event handler para los
     // distintos eventos, una vez terminado se eliminan.
     esp_event_handler_instance_t wifi_handler_event_instance;
-    ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT, // id del evento (constante del sistema)
-                                                        ESP_EVENT_ANY_ID, // cualquier evento wifi
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT,          // id del evento (constante del sistema)
+                                                        ESP_EVENT_ANY_ID,    // cualquier evento wifi
                                                         &wifi_event_handler, // pasamos el handler
-                                                        NULL, // para el campo "arg", NULL en este caso
+                                                        NULL,                // para el campo "arg", NULL en este caso
                                                         // pasamos un objeto de instancia para que el sistema lo maneje
                                                         &wifi_handler_event_instance));
 
@@ -186,33 +200,38 @@ esp_err_t connect_wifi()
 
     /** INICIAR EL DRIVER WIFI **/
     wifi_config_t wifi_config;
-    sprintf (reinterpret_cast<char*>(wifi_config.sta.ssid), WIFI_SSID );
-    sprintf (reinterpret_cast<char*>(wifi_config.sta.password), WIFI_PASS);
+    sprintf(reinterpret_cast<char *>(wifi_config.sta.ssid), WIFI_SSID);
+    sprintf(reinterpret_cast<char *>(wifi_config.sta.password), WIFI_PASS);
 
     // Iniciar el modo a "station"
-    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA) );
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
 
     // Colocamos la config.
-    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config) );
+    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config));
 
     // Iniciar el driver
     ESP_ERROR_CHECK(esp_wifi_start());
 
     /** ESPERAMOS RESPUESTAS POR PARTE DEL EVENT HANDLER **/
     EventBits_t bits = xEventGroupWaitBits(wifi_event_group,
-            WIFI_SUCCESS | WIFI_FAILURE,
-            pdFALSE,
-            pdFALSE,
-            portMAX_DELAY);
+                                           WIFI_SUCCESS | WIFI_FAILURE,
+                                           pdFALSE,
+                                           pdFALSE,
+                                           portMAX_DELAY);
 
     /* xEventGroupWaitBits() retorna los bits del evento anterior, podemos ver el resultado. */
-    if (bits & WIFI_SUCCESS) {
+    if (bits & WIFI_SUCCESS)
+    {
         ESP_LOGI(TAG, "Conectado a AP");
         status = WIFI_SUCCESS;
-    } else if (bits & WIFI_FAILURE) {
+    }
+    else if (bits & WIFI_FAILURE)
+    {
         ESP_LOGI(TAG, "Falla al conectar a AP");
         status = WIFI_FAILURE;
-    } else {
+    }
+    else
+    {
         ESP_LOGE(TAG, "Evento inesperado!");
         status = WIFI_FAILURE;
     }
@@ -225,14 +244,17 @@ esp_err_t connect_wifi()
     return status;
 }
 
+#ifndef __GNUC__
 #pragma endregion
+#endif
 
-
-
-#pragma region HANDLE_CLIENT
+#ifndef __GNUC__
+#pragma region CLIENT_HANDLER
+#endif
 
 // Enviar error al cliente, (RECV_BAD)
-void send_err(int clientSock) {
+void send_err(int clientSock)
+{
     int bytes_write = write(clientSock, RECV_BAD, RECB_SIZE);
 
     if (bytes_write < 0)
@@ -241,14 +263,28 @@ void send_err(int clientSock) {
     }
 }
 
-// Manejar el cliente luego de conectarlo
-void handle_client(int clientSock) {
+void send_ok(int clientSock)
+{
+    int bytes_write = write(clientSock, RECV_OK, RECO_SIZE);
 
-    while(1) {
-        char buf[BUF_SIZE+1] = {0};
+    if (bytes_write == -1)
+    {
+        perror("[ERRROR] al enviar RECV_OK, cliente probablemente desconectado");
+        send_err(clientSock);
+    }
+}
+
+// Manejar el cliente luego de conectarlo
+void handle_client(int clientSock)
+{
+
+    while (1)
+    {
+        char buf[BUF_SIZE + 1] = {0};
         int bytes_read = recv(clientSock, buf, BUF_SIZE, MSG_WAITALL);
 
-        if(bytes_read < 0) {
+        if (bytes_read < 0)
+        {
             send_err(clientSock);
             break;
         }
@@ -259,66 +295,90 @@ void handle_client(int clientSock) {
         char data[BUF_SIZE] = {0};
 
         // Copiar datos del buffer entre SOH y EOT
-        while(buf[i] != EOT) {
-            if(i == 0 && buf[i] == SOH) { i++; continue; }
-            if(i == 0 && buf[i] != SOH) break; // ignore packet without SOH
+        while (buf[i] != EOT)
+        {
+            if (i == 0 && buf[i] == SOH)
+            {
+                i++;
+                continue;
+            }
+            if (i == 0 && buf[i] != SOH)
+                break; // ignore packet without SOH
 
             data[j] = buf[i];
 
-            i++; j++;
+            i++;
+            j++;
         }
 
-        if( i == 0 ) continue;
+        if (i == 0)
+            continue;
 
         // Verificar el modo del cliente (REGISTER o AUTH)
-        if(strcmp(SET_MODE_REGISTER, data) == 0) {
+        // Manejar desconexiones
+        if (strcmp(SET_MODE_REGISTER, data) == 0)
+        {
             registerRoutine.setRegisterRoutine(true);
-        } else if(strcmp(SET_MODE_AUTH, data) == 0) {
+        }
+        else if (strcmp(SET_MODE_AUTH, data) == 0)
+        {
             registerRoutine.setRegisterRoutine(false);
-        } else if(strcmp(DISCONNECT, data) == 0){
+        }
+        else if (strcmp(DISCONNECT, data) == 0)
+        {
             write(clientSock, DISCONNECT, DISCONNECT_SIZE); // Enviar al cliente que nos desconectaremos
             close(clientSock);
             return;
-        } 
-        else {
-
-            switch (registerRoutine.registerMode)
-            {
-                case true:
-                    // registrar una nueva huella
-
-                    break;
-                
-                default:
-
-                    // Manejar autenticacion
-
-                    break;
-            }
-
-            // Hacer algo con la huella
         }
 
-
-        bzero(&buf, BUF_SIZE);
-
-        int bytes_write = write(clientSock, RECV_OK, RECO_SIZE);
-
-        if(bytes_write == -1)
+        // Enviar datos desde el sensor
+        else
         {
-            perror("[ERRROR] al enviar RECV_OK, cliente probablemente desconectado");
-            send_err(clientSock);
+
+            switch (registerRoutine.getRegisterRoutine())
+            {
+            case true:
+                // registrar una nueva huella
+
+                break;
+
+            default:
+
+                // Manejar autenticacion
+
+                // Recibir huella desde el sensor --> empaquetar datos --> enviar al cliente --> esperar respuesta
+
+                uint8_t *data = fingerPrintSensorRoutine.getActualFingerData();
+
+                DataPacket packet = DataPacket(data);
+
+                ssize_t bytes_write = write(clientSock, packet.data(), packet.dataSize());
+
+                if (bytes_write == -1)
+                {
+                    // Error on writing
+                    perror("writing fingerprint");
+                    send_err(clientSock);
+                    continue;
+                }
+
+                send_ok(clientSock);
+
+                break;
+            }
         }
 
+        // bzero(&buf, BUF_SIZE);
     }
-
 }
 
+#ifndef __GNUC__
 #pragma endregion
+#endif
 
-
-
+#ifndef __GNUC__
 #pragma region TCP_SERVER
+#endif
 
 // Crear servidor TCP y esperar conexiones
 esp_err_t create_tcp_server(void)
@@ -328,56 +388,57 @@ esp_err_t create_tcp_server(void)
 
     struct sockaddr_in serverInfo;
 
-	serverInfo.sin_family = AF_INET;
-	serverInfo.sin_addr.s_addr = htonl(INADDR_ANY);
-	serverInfo.sin_port = htons(SERVER_PORT);
+    serverInfo.sin_family = AF_INET;
+    serverInfo.sin_addr.s_addr = htonl(INADDR_ANY);
+    serverInfo.sin_port = htons(SERVER_PORT);
 
-	int sock = socket(AF_INET, SOCK_STREAM, 0);
-	if (sock < 0)
-	{
-		ESP_LOGE(SERVER_TAG, "Falla al crear el socket...");
-		return TCP_FAILURE;
-	}
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock < 0)
+    {
+        ESP_LOGE(SERVER_TAG, "Falla al crear el socket...");
+        return TCP_FAILURE;
+    }
 
     int status = 0;
 
-    status = bind(sock, (struct sockaddr*) &serverInfo, sizeof(serverInfo));
+    status = bind(sock, (struct sockaddr *)&serverInfo, sizeof(serverInfo));
 
-    if(status == -1)
+    if (status == -1)
     {
         ESP_LOGE(SERVER_TAG, "Falla al bindear el socket...");
         return TCP_FAILURE;
     }
 
     status = listen(sock, 10);
-    if(status == -1)
+    if (status == -1)
     {
         ESP_LOGE(SERVER_TAG, "Falla al escuchar conexiones...");
         return TCP_FAILURE;
     }
 
-    for(;;)
+    for (;;)
     {
         // La constante BUF_SIZE determina cuantos datos admitiremos por buffer, y por lectura.
         // El cliente debe enviar una cantidad igual o menor ya que no necesitamos excederla.
 
         struct sockaddr_in clientInfo;
         socklen_t len = sizeof(clientInfo);
-        int clientSock = accept(sock, (struct sockaddr*) &clientInfo, &len);
+        int clientSock = accept(sock, (struct sockaddr *)&clientInfo, &len);
 
         char readBuffer[BUF_SIZE + 1] = {'\0'};
 
         ssize_t bytes_read = read(clientSock, readBuffer, BUF_SIZE);
 
-        if(bytes_read == -1) continue;
+        if (bytes_read == -1)
+            continue;
 
         // datos en crudo, removiendo SOH y EOT
-        char data[BUF_SIZE-1] = {'\0'};
+        char data[BUF_SIZE - 1] = {'\0'};
 
         // Leer info que llega desde el cliente
         int i = 1;
         int j = 0;
-        while(readBuffer[i] != EOT)
+        while (readBuffer[i] != EOT)
         {
             // printf("0x%X ", readBuffer[i]);
             data[j] = readBuffer[i];
@@ -387,7 +448,7 @@ esp_err_t create_tcp_server(void)
 
         // Se supone que el cliente para conectarse necesita tener el passcode
 
-        if(strcmp(data, DEVICE_CODE) == 0)
+        if (strcmp(data, DEVICE_CODE) == 0)
         {
             handle_client(clientSock);
         }
@@ -402,55 +463,48 @@ esp_err_t create_tcp_server(void)
     return TCP_SUCCESS;
 }
 
+#ifndef __GNUC__
 #pragma endregion
+#endif
 
-
-
+#ifndef __GNUC__
 #pragma region MAIN
+#endif
 
 extern "C" void app_main()
 {
-	esp_err_t status = WIFI_FAILURE;
-
-    Adafruit_Fingerprint finger = Adafruit_Fingerprint(&SerialPort);
-    finger.begin(FINGERPRINT_BAUDRATE_115200);
-
-    if(finger.device_addr != 0xFFFFFFFF) {
-        ESP_LOGI("MAIN", "PASS. FINGERPRINT SENSOR (INICIADO CORRECTAMENTE)");
-    } else {
-        ESP_LOGE("MAIN", "ERROR! FINGERPRINT SENSOR (NO SE HA ENCONTRADO)");
-        // Hacer algo en caso de error
-    }
+    esp_err_t status = WIFI_FAILURE;
 
     /*
         TODO:
 
         Hay que crear una rutina que espere a que un dedo sea colocado y una vez hecho,
         envie los datos del sensor directamente hacia la PC conectada en el servidor.
-    
     */
 
-	// Formatear el NVS (Non-Volatile Storage), requerido para conectarnos a WiFi.
+    // Formatear el NVS (Non-Volatile Storage), requerido para conectarnos a WiFi.
     // Esta libreria es importante para guardar datos que necesitan
     // persistirse o ser guardados permanentemente.
     esp_err_t ret = nvs_flash_init();
-    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
-      ESP_ERROR_CHECK(nvs_flash_erase());
-      ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND)
+    {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
     }
     ESP_ERROR_CHECK(ret);
 
     // Conectar a Wifi
-	status = connect_wifi();
-	if (WIFI_SUCCESS != status)
-	{
-		ESP_LOGI(TAG, "No se pudo conectar a WiFi. Terminando...");
-		return;
-	}
+    status = connect_wifi();
+    if (WIFI_SUCCESS != status)
+    {
+        ESP_LOGI(TAG, "No se pudo conectar a WiFi. Terminando...");
+        return;
+    }
 
     // Iniciar servidor
     create_tcp_server();
-
 }
 
+#ifndef __GNUC__
 #pragma endregion
+#endif
